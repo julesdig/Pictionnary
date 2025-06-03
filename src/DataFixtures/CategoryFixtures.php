d8ca88347b6fb65f0a900738fcd9eaf8504c0164
jules <judigonnet@gmail.com>
2025-06-03 11:55:38 +0200

    fixturr

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -2,17 +2,14 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\User;
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class CategoryFixtures extends Fixture
 {
 
-    public function __construct(private readonly UserPasswordHasherInterface $userPasswordHasher)
-    {
-    }
     public function load(ObjectManager $manager): void
     {
         $categoryes = [
@@ -21,30 +18,11 @@ class CategoryFixtures extends Fixture
             "Corps humain et vêtements",
             "Nature & transport",
         ];
-
-
-
-foreach ($categoryes as $category) {
-            $categoryEntity = new Category();
-            $categoryEntity->setName($category);
-            $manager->persist($categoryEntity);
-}
-
-        $user = new User();
-        $user->setEmail("admin@gmail.com");
-        $user->setRoles(["ROLE_ADMIN"]);
-        $user->setFirstName("admin");
-        $user->setLastName("admin");
-        $user->setPassword($this->userPasswordHasher->hashPassword($user, "admin"));
-        $manager->persist($user);
-
-        $user2 = new User();
-        $user2->setEmail("user@gmail.com");
-        $user2->setRoles(["ROLE_USER"]);
-        $user2->setFirstName("user");
-        $user2->setLastName("user");
-        $user2->setPassword($this->userPasswordHasher->hashPassword($user, "user"));
-        $manager->persist($user2);
+        foreach ($categoryes as $category) {
+                    $categoryEntity = new Category();
+                    $categoryEntity->setName($category);
+                    $manager->persist($categoryEntity);
+        }
         $manager->flush();
     }
 }
